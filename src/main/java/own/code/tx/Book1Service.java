22e27bf819d36dd73e4a925ce2b896cecddda68a
wangyq <610300317@qq.com>
2020-11-25 10:48:44 +0800

    事务@Transactional

--- src/main/java/own/code/tx/Book1Service.java
+++ src/main/java/own/code/tx/Book1Service.java
@@ -2,15 +2,17 @@ package own.code.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class Book1Service {
     @Autowired
     Book1Dao book1Dao;
-
+@Transactional
     public void checkout(String username, String isbn) {
         book1Dao.updateStock(isbn);
         int price = book1Dao.getPrice(isbn);
+        int i=1/0;
         book1Dao.updateBalance(username, price);
     }
 }
