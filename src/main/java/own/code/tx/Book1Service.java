4e1c6c606c096ad160efb3a33493fd495eee2523
wangyq <610300317@qq.com>
2020-12-02 16:49:58 +0800

    事务@Transactional

--- src/main/java/own/code/tx/Book1Service.java
+++ src/main/java/own/code/tx/Book1Service.java
@@ -8,11 +8,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class Book1Service {
     @Autowired
     Book1Dao book1Dao;
-@Transactional
+
+    @Transactional(timeout = 3,readOnly = false,noRollbackFor = {ArithmeticException.class,NullPointerException.class})
     public void checkout(String username, String isbn) {
         book1Dao.updateStock(isbn);
         int price = book1Dao.getPrice(isbn);
-        int i=1/0;
+//    try {  将异常try catch起来将不会回滚
+//    } catch (Exception e) {
+//    }
+        /*try {
+            Thread.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         book1Dao.updateBalance(username, price);
+        int i = 1 / 0;
     }
 }
